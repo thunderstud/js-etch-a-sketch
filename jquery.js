c3e24a0411bbd190a60f91e95f3f518e86063870
James Lewis <ninjanuity@gmail.com>
2015-11-21 10:46:33 -0800

    update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-		for(var x = 0; x < 1; x++) {
+		for(var x = 0; x < 16; x++) {
 			for(var y = 0; y < 16; y++) {
 				var unit = $("<div class='unit'></div>");
 				unit.appendTo('#container');
